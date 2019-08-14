1494c4554088d3811066df11587f77e1651ccc97
zoraiard <zoraiarodrigues@gmail.com>
2019-08-13 23:13:20 -0300

    create request repositoy Request e RequestSate

--- src/main/java/site/zoraiarodrigues/spring/repository/UserRepository.java
+++ src/main/java/site/zoraiarodrigues/spring/repository/UserRepository.java
@@ -4,9 +4,11 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import site.zoraiarodrigues.spring.domain.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 	@Query("SELECT FROM user where email=?1 AND password =?2")
 	public Optional<User> login(String name, String password);
