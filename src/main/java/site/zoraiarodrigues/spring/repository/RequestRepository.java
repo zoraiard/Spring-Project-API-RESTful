81c81319a4869a4c810c7434b96136ca4eef05cf
zoraiard <zoraiarodrigues@gmail.com>
2019-09-10 06:25:00 -0300

    created request test

--- src/main/java/site/zoraiarodrigues/spring/repository/RequestRepository.java
+++ src/main/java/site/zoraiarodrigues/spring/repository/RequestRepository.java
@@ -1,10 +1,11 @@
 package site.zoraiarodrigues.spring.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import site.zoraiarodrigues.spring.domain.Request;
 import site.zoraiarodrigues.spring.domain.enums.RequestState;
@@ -14,7 +15,9 @@ public interface RequestRepository extends JpaRepository<Request, Long> {
 	
 	public List<Request> findAllByOwnerId(Long id);
 	
+	@Transactional(readOnly=true)
+	@Modifying
 	@Query("UPDATE request SET state = ?2 WHERE id = ?1")
-	public Request updateRequest(Long id, RequestState state);
+	public int updateStatus(Long id, RequestState state);
 	
 }
--- src/test/java/site/zoraiarodrigues/spring/repositoy/RequestRepositoryTests.java
+++ src/test/java/site/zoraiarodrigues/spring/repositoy/RequestRepositoryTests.java
@@ -27,30 +27,29 @@ public class RequestRepositoryTests {
   @Autowired private RequestRepository requestRepository;   
 
   @Test
-  public void saveText() {
+  public void AsaveText() {
 	  User owner = new User();
 	  owner.setId(1L);
 	  
-	  Request request = new Request(null, "Novo Laptop HP", "Pretendo obter um laptop HP,  ", new Date(), RequestState.OPEN, owner, null);
+	  Request request = new Request(null, "O Novo Laptop HP", "Pretendo obter um laptop HP", new Date(), 
+			  RequestState.OPEN, owner, null);
 	  
 	  Request createdRequest = requestRepository.save(request);
 	    
 	  assertThat(createdRequest.getId()).isEqualTo(1L);
   }
- 
+
   @Test
   public void updateText() {
-	  
+
 	  User owner = new User();
 	  owner.setId(1L);
 	  
-	  Request request = new Request(1L, "Novo Laptop HP", "Pretendo obter um laptop HP, de RAM 16 GB", null, RequestState.OPEN, owner, null);
-	                                     
+	  Request request = new Request(1L, "O Novo Laptop HP, teste", "Pretendo obter um laptop HP", null, 
+			  RequestState.OPEN, owner, null);
+	  
 	  Request updateRequest = requestRepository.save(request);
-	  
-	  requestRepository.save(request);
-	  
-	  assertThat(updateRequest.getDescription()).isEqualTo("Pretendo obter um laptop HP, de RAM 16 GB");
+	  assertThat(updateRequest.getId()).isEqualTo(1L);
 	  
   }
   /*
@@ -72,7 +71,14 @@ public class RequestRepositoryTests {
   public void listByOwnerIdText(){	  
 	  List<Request> requests = requestRepository.findAllByOwnerId(1L);
 	  assertThat(requests.size()).isEqualTo(1);	  
-  }  
+  }
   
-*/
+   
+  */
+  
+  @Test
+  public void updateStatusTeste() {
+	  int affectedRows = requestRepository.updateStatus(1L, RequestState.INPROGRESS);
+	  assertThat(affectedRows).isEqualTo(1);	  
+  }
 }
